d67cd3fd83f65dac19047eabc53b503a0d8fc0f5
pray4skylark <jongdals@gmail.com>
2025-12-16 15:26:46 +0900

    chore: Bump version to v3.1.0-beta.251216 (TokiView Integrated)

--- google_app_script/TokiSync/Main.gs
+++ google_app_script/TokiSync/Main.gs
@@ -1,4 +1,4 @@
-// ⚙️ TokiSync API Server v3.0.0-beta.251211 (Stateless)
+// ⚙️ TokiSync API Server v3.1.0-beta.251216 (Stateless)
 // -----------------------------------------------------
 // 🤝 Compatibility:
 //    - Client v3.0.0-beta.251211+ (User Execution Mode)
@@ -57,7 +57,7 @@ function doPost(e) {
       else if (data.type === "get_server_info") {
         result = createRes("success", {
           name: "TokiSync API",
-          version: "v3.0.0-beta.251215.0002",
+          version: "v3.1.0-beta.251216",
           url: ScriptApp.getService().getUrl(),
           user: Session.getActiveUser().getEmail(),
         });
--- tokiSyncScript.js
+++ tokiSyncScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TokiSync (Loader)
 // @namespace    https://github.com/pray4skylark/tokiSync
-// @version      3.0.0-beta.251215.0008
+// @version      3.1.0-beta.251216
 // @description  TokiSync Core Script Loader (GitHub CDN)
 // @author       pray4skylark
 // @updateURL    https://github.com/pray4skylark/tokiSync/raw/main/tokiSyncScript.js
@@ -258,7 +258,7 @@
 
                 if (typeof window.TokiSyncCore === 'function') {
                     window.TokiSyncCore({
-                        loaderVersion: "3.0.0-beta.251215.0008", // 현재 로더 버전 전달
+                        loaderVersion: "3.1.0-beta.251216", // View Migration Update
                         GM_registerMenuCommand: GM_registerMenuCommand,
                         GM_xmlhttpRequest: GM_xmlhttpRequest,
                         GM_setValue: GM_setValue,
